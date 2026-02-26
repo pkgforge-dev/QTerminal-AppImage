fb4b9ab6663339c1bc2e1d52ca09008d212bbb67
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-26 15:29:17 -0400

    add qt6ct

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm qterminal
+pacman -Syu --noconfirm qterminal qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
